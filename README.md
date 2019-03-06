b82f17f4976ac449288adcf3daf20362b4ab93ba
Lauren Sheridan <lsheridan@elon.edu>
2019-03-06 19:41:06 +0000

    work on spreadsheet download feature

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Things you may want to cover:
 * ...
 * 
 
-Readme test update
+
 
 ## Description:
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'placements_imports/new'
+  get 'placements_imports/create'
   devise_for :users
   resources :placements, :comments, :licensees
+  resources :placements_imports, only: [:new, :create]
   get '/' => 'placements#index'
 end
 
