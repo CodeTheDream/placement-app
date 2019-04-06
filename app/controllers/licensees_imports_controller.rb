6df25ef2e96e13c937e31363dd128f777846845d
Lauren Sheridan <lsheridan@elon.edu>
2019-04-06 17:50:36 +0000

    added strong params to licensees_controller

--- app/controllers/licensees_imports_controller.rb
+++ app/controllers/licensees_imports_controller.rb
@@ -8,12 +8,20 @@ class LicenseesImportsController < ApplicationController
   end
 
   def create
-    @licensees_import = LicenseesImport.new(params[:licensees_import])
-    
+    @licensees_import = LicenseesImport.new(licensees_import_params)
+    #@licensees_import = LicenseesImport.new(params[:licensees_import])
+    #byebug
     if @licensees_import.save
       redirect_to licensees_path
     else
       render :new
     end
   end
+  
+  private
+  
+    def licensees_import_params
+      params.require(:licensees_import).permit(:file)
+      #permit(:content_type, :headers, :original_filename, :tempfile)
+    end
 end
