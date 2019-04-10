2fe52913bae17e104eb91f53197372c74abcd7dc
Mike Eley <mkeley77@gmail.com>
2019-04-10 02:00:00 +0000

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 2019_03_25_021030) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
-    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
