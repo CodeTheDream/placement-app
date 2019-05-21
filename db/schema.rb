393dfd5c98c9ad73421af2102cc81901cf5e4306
Tilneil <tilneil.gary@gmail.com>
2019-05-21 16:42:33 +0000

    Added migration of columns latitude and longitude to placementst table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_143052) do
+ActiveRecord::Schema.define(version: 2019_05_21_164105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 2019_04_15_143052) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "services", force: :cascade do |t|
