c551aadad53810f95088c4187845ad4b68916756
Tilneil <tilneil.gary@gmail.com>
2019-06-10 03:18:38 +0000

    Correct commit of maps coding

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   api_key: "AIzaSyAAph3NebKd1a69uEu9XFszJ9j6rV0Z5cM",               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
+  # cache: Redis.new,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
   ip_lookup: :freegeoip, 
 
