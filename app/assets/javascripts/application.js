ed42891f1ac650e7faac42d49e4528f5216afaa7
Ubuntu <ubuntu@ip-172-31-83-153.ec2.internal>
2019-07-18 22:49:06 +0000

    add multiple markers to index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,23 @@
 // = require activestorage
 // = require_tree .
     
+    
+// function initMap() {
+//   var myLatLng = {lat: -25.363, lng: 131.044};
+
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: myLatLng
+//   });
+
+//   var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     title: 'Hello World!'
+//   });
+// }
+
+
 function initMap(lat, lng) {
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
