dc8d9524018fd49d22aea201807bce5094855944
Marcus <mdeuce@hotmail.com>
2019-10-03 14:35:32 -0400

    Added license holder attribute to user, fixed delete in admin edit view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_013926) do
+ActiveRecord::Schema.define(version: 2019_10_03_165336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_013926) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.boolean "license_holder", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
