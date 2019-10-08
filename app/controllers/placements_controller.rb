bc77b59d55ffea02660674030686fb0a873694b2
Marcus <mdeuce@hotmail.com>
2019-10-08 11:04:17 -0400

    Got tests working for placements controller / added license holder

--- app/controllers/placements_controller.rb
+++ app/controllers/placements_controller.rb
@@ -2,7 +2,7 @@
 
 class PlacementsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_admin, only: %i(new edit destroy)
+  before_action :authorize_admin, only: %i(new create edit destroy)
 
   require 'roo'
 
@@ -30,21 +30,21 @@ class PlacementsController < ApplicationController
     format.html { render :index }
     end
   end
-  
+
   def show
     @placement = Placement.find(params[:id])
     @licensee = Licensee.find(@placement.licensee_id)
     @service = Service.find(@placement.service_id)
     @comment = Comment.new(placement_id: @placement.id)
     @comments = @placement.comments.collect.sort_by {|obj| obj.created_at }.reverse
-    # Hash to build map with one marker on show page. 
+    # Hash to build map with one marker on show page.
     @hash = Gmaps4rails.build_markers(@placement) do |placement, marker|
       marker.lat placement.latitude
-      marker.lng placement.longitude  
+      marker.lng placement.longitude
       marker.infowindow placement.name
     end
   end
-  
+
   def edit
     @placement = Placement.find(params[:id])
     @licensees = Licensee.all
@@ -77,7 +77,7 @@ class PlacementsController < ApplicationController
 
     def placement_params
      params.require(:placement).permit(:name, :address, :city, :state, :zip,
-                                      :county, :phone, :licensee_id, :gender,
+                                      :county_id, :phone, :licensee_id, :service_id, :gender,
                                       :beds, :search)
     end
 end
--- spec/controllers/placements_controller_spec.rb
+++ spec/controllers/placements_controller_spec.rb
@@ -14,31 +14,48 @@ include Devise::Test::ControllerHelpers
   end
 
   it 'should create a placement' do
-    create(:licensee)
-    
-    post :create, params: {placement: {name: 'Test',
+    licensee = create(:licensee)
+    service = create(:service)
+    county = create(:county)
+
+    new_placement_params = {
+          name: 'Test',
           address: '1 New Street',
-          city: 'Raleigh', state: 'NC', zip: '11111', county: 'Wake',
-          phone: '111-111-1111', licensee_id: 1, gender: 'male', beds: 3} }
+          city: 'Raleigh',
+          state: 'NC', zip: '11111',
+          county_id: county.id,
+          phone: '111-111-1111',
+          licensee_id: licensee.id,
+          service_id: service.id,
+          gender: 'male',
+          beds: 3
+        }
+
+    post :create, params: {placement: new_placement_params }
 
     expect(response).to have_http_status(:redirect)
 
     placement = Placement.first
+
     expect(placement.name).to eq('Test')
   end
 
   it 'should update a placement' do
-    create(:licensee)
-    placement = create(:placement)
+    licensee = create(:licensee)
+    service = create(:service)
+    county = create(:county)
 
+    placement = create(:placement, licensee_id: licensee.id, service_id: service.id, county_id: county.id)
+  
     placement_params = {
       name: 'Algernon',
       address: '2 old street',
       city: 'Durham',
       state: 'NC',
-      county: 'Durham',
+      county_id: county.id,
       phone: '111-222-6666',
-      licensee_id: 1,
+      licensee_id: licensee.id,
+      service_id: service.id,
       gender: 'male',
       beds: 3
     }
